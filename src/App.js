98520e5cdb089340b4f4fac568ff0c684115e7d6
alvar91 <aleksey91scorp@bk.ru>
2023-01-26 12:57:21 +0300

    feat: done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import readXlsxFile from "read-excel-file";
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [fileData, setFileData] = useState(null);
+
+  const [searchResult, setSearchResult] = useState(null);
+
+  const importExcelData = (e) => {
+    const readFile = async () => {
+      const data = await readXlsxFile(e.target.files[0]);
+
+      setFileData(data);
+    };
+
+    readFile();
+  };
+
+  useEffect(() => {
+    if (fileData) {
+      const values = fileData.slice(1).flat();
+      setItems([...values]);
+    }
+  }, [fileData]);
+
+  const searchData = ({ target: { value } }) => {
+    if (value.length >= 3) setSearchResult(items.includes(value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <input type="file" onChange={importExcelData} />
+
+      {items.length > 0 && (
+        <input
+          className="search"
+          type="text"
+          onChange={searchData}
+          placeholder="Поиск по номеру"
+        />
+      )}
+
+      {searchResult !== null &&
+        (searchResult ? (
+          <span>номер найден</span>
+        ) : (
+          <span>номер не найден</span>
+        ))}
+
+      <ul>
+        {items?.map((item, index) => (
+          <li className="item" key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
